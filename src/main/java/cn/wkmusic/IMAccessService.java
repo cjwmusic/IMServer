4ad61505f02f2283762694e0f56988d5c5b39727
cjwmusic <caijunwei1990@163.com>
2016-09-22 09:12:17 +0800

    Add AccessService Thread.

---
+++ src/main/java/cn/wkmusic/IMAccessService.java
@@ -0,0 +1,15 @@
+package cn.wkmusic;
+
+/**
+ * Created by caijunwei on 9/22/16.
+ */
+public class IMAccessService extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+
+
+
+    }
+}
